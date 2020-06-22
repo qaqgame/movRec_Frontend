806025b6be3a29bb778f354b480396d63ce4e948
holdonbush <bleachhddh@gmail.com>
2020-06-22 09:49:21 +0800

    add vue-cli-plugin-element

---
+++ src/views/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="test">
+        <p>This is a test page</p>
+        <router-link to="/">toHome</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style scoped>
+
+</style>
