d9e807ea398772f853f1e6a1e4363224511be8f5
holdonbush <bleachhddh@gmail.com>
2020-06-23 10:27:01 +0800

    LoginCard && Login and Register switch animation

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,36 +1,68 @@
 <template>
     <div class="LoginRegister">
-        <div class="Register" :class="{CardShow: isActive}">
-            <div class="container">
-                <h2>注册</h2>
-                <h4>创建一个免费账户</h4>
-                <el-form :label-position="labelPosition" label-width="80px" :model="LoginForm" ref="LoginForm">
-                    <el-form-item prop="name" label="用户名" size="small" align="left" :rules="[
+        <transition class="transition-box" name="el-fade-in-linear">
+            <div v-if="RegisterActive" class="Register" :class="{CardShow: RegisterActive}">
+                <div class="container">
+                    <h2>注册</h2>
+                    <h4>创建一个免费账户</h4>
+                    <el-form :label-position="labelPosition" label-width="80px" :model="LoginForm" ref="LoginForm">
+                        <el-form-item prop="name" label="用户名" size="small" align="left" :rules="[
                     {required: true, message:'请输入用户名', trigger: 'blur'},
                     ]">
-                        <el-input v-model="LoginForm.name"></el-input>
-                    </el-form-item>
+                            <el-input v-model="LoginForm.name"></el-input>
+                        </el-form-item>
 
-                    <el-form-item prop="pwd" label="密码" size="small" align="left" :rules="[
+                        <el-form-item prop="pwd" label="密码" size="small" align="left" :rules="[
                     {required: true, message:'请输入密码', trigger: 'blur'},
                     ]">
-                        <el-input v-model="LoginForm.pwd"></el-input>
-                    </el-form-item>
+                            <el-input v-model="LoginForm.pwd"></el-input>
+                        </el-form-item>
 
-                    <el-form-item prop="email" label="邮箱" size="small" align="left" :rules="[
+                        <el-form-item prop="email" label="邮箱" size="small" align="left" :rules="[
                       { required: true, message: '请输入邮箱地址', trigger: 'blur' },
                       { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
                     ]">
-                        <el-input v-model="LoginForm.email"></el-input>
-                    </el-form-item>
+                            <el-input v-model="LoginForm.email"></el-input>
+                        </el-form-item>
 
-                    <el-form-item>
-                        <el-button type="primary" @click="submitForm('LoginForm')">立即创建</el-button>
-                        <el-button @click="resetForm('LoginForm')">重置</el-button>
-                    </el-form-item>
-                </el-form>
+                        <el-form-item>
+                            <el-button type="primary" @click="submitForm('LoginForm')">立即创建</el-button>
+                            <el-button @click="resetForm('LoginForm')">重置</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
             </div>
-        </div>
+        </transition>
+        <transition class="transition-box" name="el-fade-in-linear">
+            <div v-if="LoginActive" class="Login" :class="{CardShow: LoginActive}">
+                <div class="container">
+                    <h2>登录</h2>
+                    <h4>加入错影时光</h4>
+                    <el-form class="LoginForm" :label-position="labelPosition" label-width="80px" :model="LoginForm" ref="LoginForm">
+                        <el-form-item prop="name" label="用户名" size="small" align="left" :rules="[
+                    {required: true, message:'请输入用户名', trigger: 'blur'},
+                    ]">
+                            <el-input v-model="LoginForm.name"></el-input>
+                        </el-form-item>
+
+                        <el-form-item prop="pwd" label="密码" size="small" align="left" :rules="[
+                    {required: true, message:'请输入密码', trigger: 'blur'},
+                    ]">
+                            <el-input v-model="LoginForm.pwd"></el-input>
+                        </el-form-item>
+
+                        <el-form-item>
+                            <el-button type="primary" @click="submitForm('LoginForm')">立即登录</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </div>
+        </transition>
+
+        <el-row class="BtnGroup">
+            <el-button v-show="!LoginActive" @click="reverseCard()" type="primary"><i class="el-icon-arrow-left el-icon--left"></i>前往登录</el-button>
+            <el-button v-show="!RegisterActive" @click="reverseCard()" type="primary">前往注册<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+        </el-row>
     </div>
 
 </template>
@@ -40,7 +72,8 @@
         name: "LoginRegister",
         data: function () {
             return {
-                isActive: true,
+                LoginActive: true,
+                RegisterActive: false,
                 labelPosition: 'top',
                 LoginForm: {
                     name: '',
@@ -62,6 +95,10 @@
             },
             resetForm(formName) {
                 this.$refs[formName].resetFields();
+            },
+            reverseCard() {
+                this.LoginActive = !this.LoginActive;
+                this.RegisterActive = !this.RegisterActive;
             }
         }
     }
@@ -72,11 +109,16 @@
         margin: 0;
     }
 
+    .LoginRegister {
+        position: relative;
+    }
+
     .CardShow {
         height: 450px;
         width: 360px;
         background-color: whitesmoke;
         border-radius: 15px;
+        position: absolute;
     }
 
     .CardShow h2 {
@@ -96,5 +138,18 @@
     .container {
         width: 280px;
         margin: 0 auto;
+    }
+
+    .LoginForm {
+        margin-top: 50px;
+    }
+
+    .BtnGroup {
+        position: absolute;
+        margin-top: 470px;
+    }
+
+    .el-button+.el-button{
+        margin-left: 0!important;
     }
 </style>
