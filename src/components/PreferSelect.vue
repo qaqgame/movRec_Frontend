3ce7763df70016bbbd782e3fc4d2f476c29425eb
SakuraGranger <379892527@qq.com>
2020-07-05 12:06:08 +0800

    add component-Prefer

--- src/components/PreferSelect.vue
+++ src/components/PreferSelect.vue
@@ -1,46 +1,57 @@
 <template>
     <div>
         <h1>请选择你的偏好</h1>
-        <div style="text-align: center">
-            <el-transfer
-                    style="text-align: left; display: inline-block"
-                    v-model="selected"
-                    :titles="['电影类型','已选择']"
-                    :format="{
-                    noChecked:'${total}',
-                    hasChecked:'${checked}/${total}'
-                }"
-                    :data="data">
-                <span slot-scope="{option}">{{option.key}}</span>
-            </el-transfer>
-            <div>
-                <el-button>确认</el-button>
-            </div>
-
-        </div>
+        <el-col :span="12">
+            <el-row>
+                <el-button v-for="(tps,index) in fTypes" :key="index" @click="changeStatus(index)"
+                           style="font-size: 60px;margin: 1% 1%" round>
+                    {{tps}}
+                </el-button>
+            </el-row>
+        </el-col>
+        <el-col :span="12">
+            <el-row>
+                <el-button v-for="tps in selectedList" :key="tps" @click="cancelSelected(tps)"
+                           style="font-size: 60px;margin: 1% 1%" round>
+                    {{tps}}
+                </el-button>
+            </el-row>
+        </el-col>
     </div>
 </template>
 
 <script>
-    const filmTypes = ['古装', '动画', '冒险', '犯罪', '奇幻', '家庭', '恐怖', '传记', '科幻', '惊悚', '爱情', '历史', '歌舞',
-        '战争', '动作', '舞台艺术', '西部', '音乐', '悬疑', '运动', '儿童', '武侠', '灾难', '戏曲', '情色', '短片', '喜剧', '荒诞']
+    const filmTypes = ['古装', '动画', '冒险', '犯罪', '奇幻', '家庭', '恐怖', '传记', '科幻', '惊悚', '爱情', '历史', '歌舞', '战争', '动作', '舞台艺术', '西部', '音乐', '悬疑', '运动', '儿童', '武侠', '灾难', '戏曲', '情色', '短片', '喜剧', '荒诞']
     export default {
         name: "PreferSelect",
         data() {
-            const generateData = _ => {
-                const data = [];
-                for (let i = 0; i < filmTypes.length; i++) {
-                    data.push({
-                        key: filmTypes[i],
-                        label: filmTypes[i]
-                    });
-                }
-                return data;
-            }
             return {
-                data: generateData(),
-                selected: []
-            };
+                fTypes: filmTypes,
+                selectedList: [],
+                btnStatus: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+            }
+        },
+        computed: {},
+        methods: {
+            changeStatus: function (index) {
+                // 存在返回下标，不存在返回-1
+                let num = this.selectedList.indexOf(this.fTypes[index])
+                // 如果当前数据不存在selectedList中，表明没有被点击过，添加
+                if (num === -1) {
+                    this.selectedList.push(this.fTypes[index])
+                }
+                // this.changeBtnStatus(index)
+                console.log(this.selectedList)
+            },
+            cancelSelected: function (tps) {
+                let num = this.selectedList.indexOf(tps)
+                this.selectedList.splice(num, 1)
+                console.log('Remove' + tps)
+            }
+            // changeBtnStatus: function (index) {
+            //     this.btnStatus[index] = true;
+            //     return this.btnStatus[index]
+            // }
         }
     }
 </script>
