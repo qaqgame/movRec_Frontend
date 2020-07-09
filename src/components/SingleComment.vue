6350466c3f5d6a95f9dd16dfd087b005f026a58f
holdonbush <bleachhddh@gmail.com>
2020-07-09 09:05:34 +0800

    update

--- src/components/SingleComment.vue
+++ src/components/SingleComment.vue
@@ -66,7 +66,7 @@
                 circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
                 size:this.headSize,
                 replydata: this.replyData,
-                value: this.replyData.score,
+                value: this.replyData.score/2,
                 isActive:true,
                 textarea: '',
                 replytoreply:false,
